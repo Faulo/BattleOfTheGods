5fea3ad509fb2a08a7ca23f9d998556fd0bd4e9a
Paul Redetzky <p_redetzky@gmx.de>
2022-01-29 00:51:08 +0100

    some stuff

--- Assets/Scripts/Cards/CardInstance.cs
+++ Assets/Scripts/Cards/CardInstance.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
+using System;
 namespace Runtime.Cards
 {
-    public class CardInstance : MonoBehaviour
-    {
+    public class CardInstance : MonoBehaviour, IPointerClickHandler {
+
+        public static event Action<CardInstance> clicked;
+
         public CardData data;
         public int cost => data.cost;
         public List<CardEffect> effects => new List<CardEffect>(data.effects);
@@ -15,6 +18,10 @@ namespace Runtime.Cards
 
         public void Init(CardData data) {
             this.data = data;
+        }
+
+        public void OnPointerClick(PointerEventData eventData) {
+            clicked?.Invoke(this);
         }
     }
 }
