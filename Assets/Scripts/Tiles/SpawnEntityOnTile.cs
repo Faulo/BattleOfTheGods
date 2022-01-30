a80a4bf3162f080a2236b61a1141b63e7d8e901a
Daniel Schulz <faulolio@gmail.com>
2022-01-30 13:11:36 +0100

    Add GridGizmo script

--- Assets/Scripts/Tiles/SpawnEntityOnTile.cs
+++ Assets/Scripts/Tiles/SpawnEntityOnTile.cs
@@ -27,14 +27,5 @@ namespace Runtime.Tiles {
             }
             World.instance.InstantiateEntity(tile, entityType);
         }
-
-        /*
-        protected void OnDrawGizmos() {
-            foreach (var position in World.instance.GetNeighboringPositions(World.instance.WorldToGrid(transform.position))) {
-                Gizmos.color = Color.cyan;
-                Gizmos.DrawWireSphere(World.instance.GridToWorld(position), 0.5f);
-            }
-        }
-        //*/
     }
 }
