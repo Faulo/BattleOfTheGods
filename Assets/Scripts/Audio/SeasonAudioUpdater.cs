4343a2c3d66cc8e8164a63057c8e030a58304d10
Paul Redetzky <p_redetzky@gmx.de>
2022-01-30 11:34:08 +0100

    Added season audio updater

---
+++ Assets/Scripts/Audio/SeasonAudioUpdater.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMOD.Studio;
+namespace Runtime.Audio {
+    public class SeasonAudioUpdater : MonoBehaviour {
+
+        PARAMETER_DESCRIPTION seasonsDescription;
+        PARAMETER_ID seasonsID;
+        // Start is called before the first frame update
+        void Start() {
+            FMODUnity.RuntimeManager.StudioSystem.getParameterDescriptionByName("Seasons", out seasonsDescription);
+            seasonsID = seasonsDescription.id;
+            
+        }
+        void OnSeasonChange() {
+
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByID(seasonsID, (int)World.instance.season);
+        }
+
+    }
+}
