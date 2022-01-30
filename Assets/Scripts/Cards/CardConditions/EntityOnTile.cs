fc4fe9a9233ef65c8a385c276b3dc3f223044290
Daniel Schulz <faulolio@gmail.com>
2022-01-30 01:18:55 +0100

    Fix EntityOnTile

--- Assets/Scripts/Cards/CardConditions/EntityOnTile.cs
+++ Assets/Scripts/Cards/CardConditions/EntityOnTile.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Runtime.Entities;
 using Slothsoft.UnityExtensions;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Runtime.Cards.CardConditions {
     [CreateAssetMenu(fileName = "IsEntityOnTile.asset", menuName = "Card Conditions/Entity On Tile")]
@@ -10,10 +11,9 @@ namespace Runtime.Cards.CardConditions {
         [SerializeField, Expandable]
         EntityData[] requiredEntities = Array.Empty<EntityData>();
         public override bool Check(PlayConditionData data) {
-            if (requiredEntities.Length <= 0)
-                return false;
+            Assert.AreNotEqual(requiredEntities.Length, 0, $"Needs entities for {this}");
 
-            return requiredEntities.Length <= 0 || requiredEntities.All(e => data.cell.entities.Any(entityOnCell => e == entityOnCell.type));
+            return requiredEntities.All(e => data.cell.entities.Any(entityOnCell => e == entityOnCell.type));
         }
     }
 }
