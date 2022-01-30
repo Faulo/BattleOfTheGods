050c76d90d776576613bdb0ac25812bac5514d30
Daniel Schulz <faulolio@gmail.com>
2022-01-30 15:01:49 +0100

    Implement memory

--- Assets/Scripts/Entities/MoveToClosestEntity.cs
+++ Assets/Scripts/Entities/MoveToClosestEntity.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Slothsoft.UnityExtensions;
 using UnityEngine;
@@ -6,9 +7,14 @@ namespace Runtime.Entities {
     public class MoveToClosestEntity : MonoBehaviour {
         [SerializeField, Expandable]
         EntityData targetType = default;
+        [SerializeField]
+        int minimumDistanceToTarget = 0;
+        [SerializeField]
+        bool rememberPreviousEntities = false;
 
         bool isInitialized = false;
         IEntity entity;
+        readonly HashSet<IEntity> visitedEntities = new HashSet<IEntity>();
 
         protected void Start() {
             isInitialized = true;
@@ -20,14 +26,31 @@ namespace Runtime.Entities {
                 return;
             }
 
-            var targetEntity = World.instance
+            var candidates = World.instance
                 .GetEntitiesByType(targetType)
+                .ToList();
+
+            if (rememberPreviousEntities) {
+                if (candidates.All(visitedEntities.Contains)) {
+                    visitedEntities.Clear();
+                } else {
+                    foreach (var visited in visitedEntities) {
+                        candidates.Remove(visited);
+                    }
+                }
+            }
+
+            var targetEntity = candidates
                 .OrderBy(targetEntity => World.Distance(entity.gridPosition, targetEntity.gridPosition))
                 .FirstOrDefault();
             if (targetEntity != null) {
-                if (World.instance.TryCalculatePath(entity.gridPosition, targetEntity.gridPosition, out var path)) {
+                if (World.instance.TryCalculatePath(entity.gridPosition, targetEntity.gridPosition, out var path) && path.Count > minimumDistanceToTarget) {
                     // Debug.Log($"{entity}: {string.Join(" > ", path)}");
                     World.instance.MoveEntity(entity, path[0]);
+                } else {
+                    if (rememberPreviousEntities) {
+                        visitedEntities.Add(targetEntity);
+                    }
                 }
             }
         }
